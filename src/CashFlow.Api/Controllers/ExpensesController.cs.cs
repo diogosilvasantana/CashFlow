f439bddf053edf0bfddcdaf721e4a687335d38f6
Diogo SIlva <diogosilvaoficial@gmail.com>
2024-08-03 11:36:01 -0300

    first commit

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController.cs : ControllerBase
+    {
+    }
+}
